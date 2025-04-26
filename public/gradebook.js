78cf3468cebda07fc1c92f768328ad6a8fe919e3
schope26 <schope@my.waketech.edu>
2025-04-26 12:33:22 -0400

    Update gradebook.js

--- public/gradebook.js
+++ public/gradebook.js
@@ -8,19 +8,19 @@ function fetchGradeData() {
     let apiRoute = "/api/grades";
     // When the request changes status, we run this anonymous function
     xhr.onreadystatechange = function(){
-        let results;
         // Check if were done
-        if(xhr.readyState === xhr.DONE){
+        if(xhr.readyState === 4){
             // Check if were successful
             if(xhr.status !== 200){
                 console.error(`Could not get grades.
                     Status: ${xhr.status}`)
+                    return;
             }
             // And then call the function to update the HTML with our data
             populateGradebook(JSON.parse(xhr.responseText))
         }
     }.bind(this);
-    xhr.open("get", apiRoute, true);
+    xhr.open("GET", apiRoute, true);
     xhr.send();
 }
 
@@ -31,7 +31,7 @@ function populateGradebook(data) {
     let tableElm = document.getElementById("gradebook"); // Get the gradebook table element
         data.forEach(function(assignment){
             let row = document.createElement('tr'); // Create a table row element
-            let columns = []; // Handy place to stick the columns of information
+            let columns = {}; // Handy place to stick the columns of information
             columns.name = document.createElement('td'); // The first columns table data will be the name
             columns.name.appendChild(
                 // Concatenate the full name:  "last_name, first_name"
